c45d52773cf6714e28c5bcc1b0fa4802e2087ea9
Tatsuya Yagi <t_yagi@mebius.co.jp>
2022-10-13 18:29:32 +0900

    10/13の実施前メモ

--- src/section10.ts
+++ src/section10.ts
@@ -163,4 +163,139 @@ namespace A {
 }
 
 import d = A.B1
+import { resolve } from 'path';
+import { listenerCount } from 'process';
+
+
+// モジュールモードとスクリプトモード
+
+// モジュールモード
+// import とか import() を使って他のファイルからコードを要求し、exportを使って
+// 他のファイルでもコードを利用できるようにする
+
+// スクリプトモード
+// プログラマが宣言するトップレベルの変数は、明示的なインポートがなくても、
+// プロジェクト内の他のファイルから利用可能
+
+
+
+/**
+ * 名前空間
+ * 
+ * コードをカプセル化するための方法の一つ
+ * 名前空間はファイルシステム内でファイルがどのように配置されるかという詳細を抽象化する
+ * 
+ * .mine関数がschemes/scams/bitcoin/appsフォルダの中に存在することを知る必要なく
+ * Schemes.Scams.Bitcoin.Apps.mine のような名前空間を使って関数にアクセスする
+ */
+
+
+// Get.ts
+namespace Network {
+	export function get<T>(url: string):Promise<T> {
+		// ～
+		return new Promise((resolve, reject) => {
+
+		});
+	}
+}
+
+// App.ts
+namespace App {
+	Network.get<GitRepo>('https://api.github.com/repos/Microsoft/typescript')
+}
+
+
+// 名前空間はNetworkのように名前を持つ必要がある
+// 関数、変数、型、インターフェース、別の名前空間をエクスポートすることができる
+
+// モジュールが大きくなってきたら、サブモジュールに分割することができる
+
+namespace Network {
+	export namespace HTTP {
+		export function get<T>(url: string):Promise<T> {
+			// ～
+			return new Promise((resolve, reject) => {
+			});
+		}	
+	}
+
+	export namespace TCP {
+		export function listenOn(port: number): Connection {
+			// ・・・
+		}
+	}
+
+	export namespace UDP {
+		// ・・・
+	}
+}
+
+// 別々に宣言しても、よろしくマージしてくれる
+namespace Network {
+	export namespace IP {
+		// ・・・
+	}
+}
+
+
+// 階層が長くなってきたら、名前空間エイリアスで短縮して使うことができる
+
+// MyApp.ts
+import httpGet = Network.HTTP.get // エイリアスを定義
+
+let repo: Promise<GitRepo> = httpGet<GitRepo>('https://api.github.com/repos/Microsoft/typescript');
+
+
+
+// 同じ名前のエクスポートの競合は許されない
+namespace Network {
+	export function request<T>(url: string): T {
+		// ・・・
+	}
+}
+
+namespace Network {
+	export function request<T>(url: string) { // ←おこられる
+		// ・・・
+	}
+}
+
+
+// コンパイルされたときの出力
+
+// Flowers.ts
+// namespace Flowers {
+// 	export function give(count: number) {
+// 		return count + ' flowers'
+// 	}
+// }
+
+// 上記ファイルをTSCでコンパイルすると
+
+(function (Flowers) {
+	function give(count) {
+		return count + ' flowers'
+	}
+
+	Flowers.give = give
+})(Flowers || (Flowers = {}))
+
+
+// と、ここまで書いてきたが 可能であれば名前空間よりモジュールを使ったほうがいいらしい
+// 明示的な依存関係には、読みやすさ、モジュールの分離の強制、および静的解析に関して
+// 多くのメリットがある
+
+// 大きなフロントエンドプロジェクトでは特に重要になる
+
+
+
+/**
+ * 宣言のマージ
+ * 
+ * これまでTypeScriptが行う3種類のマージについて触れてきている
+ * ① 値の型のマージ（コンパニオンオブジェクトパターン）
+ * ② 同じ名前を持つ複数の名前空間のマージ
+ * ③ 同じ名前を持つ複数のインターフェースのマージ
+ */
 
